8cb1e273604c0a07b4fdd31a425a9dc5ea423204
Ram <Ram@DESKTOP-PODF5JG>
2026-02-22 21:08:44 +0530

    Update Flask app to Version 5

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """Return a welcome message for the DevOps Lab."""
-    return "Welcome to DevOps Lab - Version 4"
+    return "Welcome to DevOps Lab - Version 5"
 
 
 def open_browser():
